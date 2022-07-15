1eed75c1bfac5fcb7ba0d9be30f1896c30a74002
MoseyByrne <mosey.b.byrne@gmail.com>
2022-07-15 14:31:33 -0700

    pass special tests

---
+++ Bakery.Tests/ModelTests/SpecialTests.cs
@@ -0,0 +1,30 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bakery.Models;
+
+namespace Bakery.Tests
+{
+  [TestClass]
+  public class SpecialTests
+  {
+    
+    [TestMethod]
+    public void SpecialCost_CostOneSpecial_Int()
+    {
+      int specialResponse = 1;
+      int specialCost = 4;
+      
+      Special newSpecial = new Special (specialResponse);
+      Assert.AreEqual(specialCost, newSpecial.SpecialCost());
+    }
+
+        [TestMethod]
+    public void SpecialCost_CostFiveSpecial_Int()
+    {
+      int specialResponse = 5;
+      int specialCost = 20;
+      
+      Special newSpecial = new Special (specialResponse);
+      Assert.AreEqual(specialCost, newSpecial.SpecialCost());
+    }
+  }
+  }
