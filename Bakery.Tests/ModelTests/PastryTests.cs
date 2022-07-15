a63ce6faa6b766de5bc1a419a938efe2ac26988a
MoseyByrne <mosey.b.byrne@gmail.com>
2022-07-15 13:28:04 -0700

    add readme

--- Bakery.Tests/ModelTests/PastryTests.cs
+++ Bakery.Tests/ModelTests/PastryTests.cs
@@ -1,11 +1,57 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Bakery;
+using Bakery.Models;
 
 namespace Bakery.Tests
 {
   [TestClass]
   public class PastryTests
   {
-    // Test methods go here
+    
+    [TestMethod]
+    public void PastryCost_CostOnePastry_Int()
+    {
+      int pastryResponse = 1;
+      int pastryCost = 2;
+      Pastry newPastry = new Pastry (pastryResponse);
+      Assert.AreEqual(pastryCost, newPastry.PastryCost());
+    }
+
+
+    [TestMethod]
+    public void PastryCost_CostOfTwoPastries_Int()
+    {
+      int pastryResponse = 2;
+      int pastryCost = 4;
+      Pastry newPastry = new Pastry(pastryResponse);
+      Assert.AreEqual(pastryCost, newPastry.PastryCost());
+    }
+
+    [TestMethod]
+    public void PastryCost_ThirdPastryIsFree_Int()
+    {
+      int pastryResponse = 3;
+      int pastryCost = 5;
+      Pastry newPastry = new Pastry(pastryResponse);
+      Assert.AreEqual(pastryCost, newPastry.PastryCost());
+    }
+
+    [TestMethod]
+    public void PastryCost_CostOfFourPastries_Int()
+    {
+      int pastryResponse = 4;
+      int pastryCost = 7;
+      Pastry newPastry = new Pastry(pastryResponse);
+      Assert.AreEqual(pastryCost, newPastry.PastryCost());
+    }
+
+
+    [TestMethod]
+    public void PastryCost_SixthPastryIsFree_Int()
+    {
+      int pastryResponse = 6;
+      int pastryCost = 10;
+      Pastry newPastry = new Pastry(pastryResponse);
+      Assert.AreEqual(pastryCost, newPastry.PastryCost());
+    }
   }
 }
---
+++ README.md
@@ -0,0 +1,34 @@
+
+# Pierre's Bakery
+
+#### By Mo Byrne
+
+#### A C# console program for ordering bread and pastries
+
+## Technologies Used
+
+* C#
+* Markdown
+
+## Description
+
+Welcomes the user to the bakery. Displays prices and deals for pasties and loaves of bread, then asks how many the user wants to order. Returns the orders total.
+
+## Setup/Installation Requirements
+
+* Setup requires dotnet core to be installed.
+* Visit my GitHub repository at https://github.com/MoseyByrne/PierresBakery and clone the repository (git clone https://github.com/MoseyByrne/PierresBakery)
+* In terminal, navigate to bakery directory and run "dotnet build" to build program.
+* In terminal, run "dotnet run" to run program.
+* Open files in text editor to view code.
+
+
+## Known Bugs
+
+* No known bugs
+
+## License
+
+MIT
+
+Copyright (c) 07/15/2022 Mo Byrne
