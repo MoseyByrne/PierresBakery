92fd7e12611ca8e459380ebf7ee6090b81fe62da
MoseyByrne <mosey.b.byrne@gmail.com>
2022-07-15 11:13:42 -0700

    bread tests pass

--- Bakery.Tests/ModelTests/BreadTests.cs
+++ Bakery.Tests/ModelTests/BreadTests.cs
@@ -1,11 +1,55 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Bakery;
+using Bakery.Models;
 
 namespace Bakery.Tests
 {
   [TestClass]
   public class BreadTests
   {
-    // Test methods go here
+    [TestMethod]
+    public void BreadCost_CostOneLoaf_Int()
+    {
+      int loafResponse = 1;
+      int breadCost = 5;
+      Bread newBread = new Bread(loafResponse);
+      Assert.AreEqual(breadCost, newBread.BreadCost());
+    }
+
+    [TestMethod]
+    public void BreadCost_CostOfTwoLoaves_Int()
+    {
+      int loafResponse = 2;
+      int breadCost = 10;
+      Bread newBread = new Bread(loafResponse);
+      Assert.AreEqual(breadCost, newBread.BreadCost());
+    }
+
+    [TestMethod]
+    public void BreadCost_ThirdLoafIsFree_Int()
+    {
+      int loafResponse = 3;
+      int breadCost = 10;
+      Bread newBread = new Bread(loafResponse);
+      Assert.AreEqual(breadCost, newBread.BreadCost());
+    }
+
+    [TestMethod]
+    public void BreadCost_CostOfFourLoaves_Int()
+    {
+      int loafResponse = 4;
+      int breadCost = 15;
+      Bread newBread = new Bread(loafResponse);
+      Assert.AreEqual(breadCost, newBread.BreadCost());
+    }
+
+
+    [TestMethod]
+    public void BreadCost_SixthLoafIsFree_Int()
+    {
+      int loafResponse = 6;
+      int breadCost = 20;
+      Bread newBread = new Bread(loafResponse);
+      Assert.AreEqual(breadCost, newBread.BreadCost());
+    }
   }
 }
